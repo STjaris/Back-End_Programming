f01798a344bcc0653f8def1c66069b2a627abb47
STjaris <43371513+STjaris@users.noreply.github.com>
2020-11-16 12:43:34 +0100

    initial push

---
+++ src/test/java/com/example/BackEnd_Programming/BackEndProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEnd_Programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
