650f9c2a6735d8c147a70f96c5f68bed223b89e8
STjaris <43371513+STjaris@users.noreply.github.com>
2020-11-19 11:21:00 +0100

    Char check test update

--- src/test/java/com/example/BackEnd_Programming/textimport/textimportTests.java
+++ src/test/java/com/example/BackEnd_Programming/textimport/textimportTests.java
@@ -22,13 +22,34 @@ public class textimportTests {
         );
     }
 
+    private static Stream<Arguments> provideCharAndResults() {
+        return Stream.of(
+                Arguments.of("TEST", false),
+                Arguments.of("@test", false),
+                Arguments.of("123456", false),
+                Arguments.of("/,.\\", false),
+                Arguments.of("qwerty", true)
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("provideLengthAndResults")
     @DisplayName("Is in between 5 and 7")
-    public void verifyIsInBetween(String input, boolean expectedResult) {
+    public void verifyLengthIsInBetween(String input, boolean expectedResult) {
         textimport textImport = new textimport();
 
         boolean result = textImport.stringFilteringOnLength(input);
+
+        assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCharAndResults")
+    @DisplayName("Contains no special characters")
+    public void verifyCharOfString(String input, boolean expectedResult) {
+        textimport textImport = new textimport();
+
+        boolean result = textImport.stringFilteringOnSpecialChar(input);
 
         assertEquals(expectedResult, result);
     }
